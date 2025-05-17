20a33fc08e69bed334795d56d98fdfaff88bf8d8
Johannes Røsvik <j.rosvik@gmail.com>
2025-05-17 23:35:22 +0200

    log listening addr after startup

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let port = match args.iter().position(|x| x == "--port") {
         Some(i) => args[i + 1].clone(),
-        None => "2331".to_string(),
+        None => String::from("2331"),
     };
 
     let addr = format!("0.0.0.0:{}", port);
-    println!("Listening on http://{}", addr);
     let serv = Server::http(addr).expect("Failed to start server");
+    println!("Listening on {}", serv.server_addr());
 
     for req in serv.incoming_requests() {
         let res = ip_res(&req);
